e8f71bc828a6cb2f6001df8304c65cf52a679496
TakamiChie@Visnu <chie@onpu-tamago.net>
2020-09-24 09:06:28 +0900

    Implemented a simple calculator function.

--- samples/calc.py
+++ samples/calc.py
@@ -5,7 +5,42 @@ sys.path.append(str(Path(__file__).parent.parent))
 from tksugar import Generator
 
 def calcbutton(button, tag):
-  manager.vars["label"].set(button["text"])
+  text = button["text"]
+  var = manager.vars["label"]
+  calctext = var.get()
+  result = ""
+  if text == "C":
+    # Clear.
+    if len(calctext) > 0:
+      result = calctext[:-1]
+  elif text == "AC":
+    # All clear.
+    result = ""
+  elif text == "=":
+    # Set the calculation result.
+    try:
+      result = eval(calctext)
+    except SyntaxError:
+      pass
+  elif text == "%":
+    # Multiply the calculation result by 0.01
+    if len(calctext) == 0:
+      return
+    if calctext[-1] in [".", "+", "-", "*", "/"]:
+      calctext = calctext[:-1]
+    var.set(calctext + "*0.01")
+    calcbutton(manager.widgets["equal"].widget, manager.widgets["equal"]) # Perfome Click.
+    return
+  else:
+    # Do not press the button if the operator or calculation string is empty immediately before.
+    if text in ["+", "-", "*", "/"]:
+      if calctext == "" or calctext[-1] in ["+", "-", "*", "/"]:
+        return
+    # Do not allow multiple periods to be entered.
+    if text == "." and "." in calctext:
+      return
+    result = calctext + str(text)
+  var.set(result)
 
 if __name__ == "__main__":
   gen = Generator("samples\yml\calc.yml")
