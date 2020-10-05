1493cca2396e2c6e43ba651f3bd2194ed1c8bc24
TakamiChie@Visnu <chie@onpu-tamago.net>
2020-10-05 10:53:17 +0900

    Implemented sample folder selection dialog and file save dialog.

--- samples/filerefer.py
+++ samples/filerefer.py
@@ -6,11 +6,18 @@ sys.path.append(str(Path(__file__).parent.parent))
 from tksugar import Generator
 
 def referbutton(button, tag):
+  tagname = tag.tag["tag"]
   folder = Path(__file__).parent
   filters = [("All Files", "*.*")]
-  n = tkinter.filedialog.askopenfilename(filetypes = filters, initialdir = folder)
+  n = ""
+  if tagname == "file":
+    n = tkinter.filedialog.askopenfilename(filetypes = filters, initialdir = folder)
+  elif tagname == "dir":
+    n = tkinter.filedialog.askdirectory(initialdir = folder)
+  elif tagname == "save":
+    n = tkinter.filedialog.asksaveasfilename(filetypes = filters, initialdir = folder)
   if n != "":
-    manager.vars[tag.tag["tag"]].set(n)
+    manager.vars[tagname].set(n)
 
 if __name__ == "__main__":
   gen = Generator(r"samples\yml\filerefer.yml")
